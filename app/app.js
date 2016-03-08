bef6032bffdd6213e45d0abd1d229e698e672f85
Anh Bui <b.ducanh96@gmail.com>
2016-03-08 01:16:24 -0500

    nitpicking

--- app/app.js
+++ app/app.js
@@ -6,9 +6,6 @@ import { IndexRoute, Router, Route, browserHistory } from 'react-router'
  * The primary component in our application.
  * The Router will give it different child Components as the user clicks
  * around the application.
- *
- * If we implemented all of Facebook, this App would also contain Component
- * objects for the left and right content panes.
  */
 class App extends React.Component {
   render() {
