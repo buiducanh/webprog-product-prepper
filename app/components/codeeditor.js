d31a89bd25973878e43f7fb2e7b892a67c663af4
Thanh Pham <tphamcs@gmail.com>
2016-03-13 10:26:48 -0400

    update interview.js

---
+++ app/components/codeeditor.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+export default class CodeEditor extends React.Component {
+  render() {
+    return(
+        <div className="col-md-8">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+
+                <button type="button" className="btn btn-default" style={{height: '35px'}} >
+                  <p> Language
+                    <select>
+                      <option value="Python"> Python </option>
+                      <option value="Java"> Java </option>
+                      <option value="PHP"> C </option>
+                    </select>
+                  </p>
+                </button>
+
+                <Link to={"/feedback"}>
+                  <button  type="button" className="btn btn-default" onClick={(e) => this.handleSubmit(e)}>
+                    <span className="glyphicon glyphicon-ban-circle"></span>  End Interview
+                  </button>
+                </Link>
+
+
+            </div>
+            <div className="panel-body" style={{height: '490px'}}>
+            Code Editor
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
