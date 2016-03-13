b83ca5d22ecf24374a52c36cbe61e45745b75a65
Tri Nguyen <nghgminhtri@gmail.com>
2016-03-13 12:47:13 -0400

    create dynamic content for feedback

--- app/components/history.js
+++ app/components/history.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PastInterviewsList from "./pastinterviewslist";
-import InterviewFeedback from "./interviewfeedback";
 
 export default class History extends React.Component {
   render() {
     return (
       <div className="history container">
         <div className="row">
-          <PastInterviewsList/>
-          <InterviewFeedback/>
+          <PastInterviewsList user={this.props.route.user}/>
         </div>
       </div>
     )
--- app/components/pastinterviewslist.js
+++ app/components/pastinterviewslist.js
@@ -1,20 +1,48 @@
 import React from 'react';
-
+import InterviewFeedback from "./interviewfeedback";
+import {getInterviewData} from '../server'
+import {unixTimeToString} from '../util'
 export default class PastInterviewsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {interview: [],selected: null};
+  }
+  refresh() {
+    getInterviewData(this.props.user, (interviewData) => {
+      this.setState({interview: interviewData,selected:interviewData[0]});
+    });
+  }
+  componentDidMount() {
+    this.refresh();
+  }
   render() {
+    var selectedStyle = {"listStyleType":"disc","fontWeight": "bold"};
     return (
-      <div className="col-md-4">
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <div className="row part-header">Your past mock interview</div>
-            <hr style={{"marginTop": "2px","marginBottom": "2px","borderTop": "1px solid gray"}}></hr>
-            <ul className="mock-list">
-              <a href="#"><li className="mock-list-element"> 02/02/16 - 19:30 </li> </a>
-              <a href="#"><li className="mock-list-element" style={{"listStyleType":"disc","fontWeight": "bold"}}> 02/03/16 - 09:30 </li> </a>
-              <a href="#"><li className="mock-list-element"> 02/04/16 - 13:45 </li> </a>
-            </ul>
+      <div>
+        <div className="col-md-4">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <div className="row part-header">Your past mock interview</div>
+              <hr style={{"marginTop": "2px","marginBottom": "2px","borderTop": "1px solid gray"}}></hr>
+              <ul className="mock-list">
+                {
+                  this.state.interview.map((interview, i) => {
+                    // i is comment's index in comments array
+                    if (interview._id==this.state.selected._id)
+                      return (
+                        <a key = {i} href="#"><li style={selectedStyle} className="mock-list-element"> {unixTimeToString(interview.feedback.timestamp)} </li> </a>
+                      );
+                    else
+                      return (
+                        <a key = {i} href="#"><li className="mock-list-element"> {unixTimeToString(interview.feedback.timestamp)} </li> </a>
+                      );
+                  })
+                }
+              </ul>
+            </div>
           </div>
         </div>
+        <InterviewFeedback data={this.state.selected} />
       </div>
     )
   }
