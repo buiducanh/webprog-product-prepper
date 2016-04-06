9d698ca3812475d88386efb8cf1cc6163b4bb181
Thanh Pham <tphamcs@gmail.com>
2016-04-05 22:15:25 -0400

    interviews.js

--- client/app/components/interview.js
+++ client/app/components/interview.js
@@ -5,6 +5,19 @@ import CodeEditor from "./codeeditor";
 
 
 export default class Interview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {interview: []};
+  }
+  refresh() {
+    getInterviewData(this.props.idInterview, (interviewData) => {
+      this.setState({interview: interviewData});
+    });
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
 
   render() {
     return (
@@ -12,7 +25,11 @@ export default class Interview extends React.Component {
         <div className="row">
           <div className="col-md-4">
             <VoiceChat />
-            <Questions />
+              {
+                this.state.interview.map((interview, i) => {
+                  return (<Questions key={i} user={this.state.route.user} interviewquestion={interview}/>);
+                })
+              }
           </div>
           <CodeEditor interviewerId={this.props.params.interviewerId} intervieweeId={this.props.params.intervieweeId} />
         </div>
--- client/app/components/interviewquestions.js
+++ client/app/components/interviewquestions.js
@@ -4,12 +4,14 @@ import React from 'react';
 
 export default class Questions extends React.Component {
   render() {
+    var data = this.props.interviewquestion;
     return (
           <div className="panel panel-default">
             <div className="panel-heading">
               <h4>Interview Quesions</h4>
                 <div>
-
+                    {data.problem.title};
+                    {data.problem.question};
                 </div>
             </div>
           </div>
