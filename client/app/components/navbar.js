d65071253611128a7424081e8bcefc2fd1c1b0f1
Anh Bui <b.ducanh96@gmail.com>
2016-04-25 02:32:42 -0400

    fix navbar container

--- client/app/components/navbar.js
+++ client/app/components/navbar.js
@@ -72,7 +72,7 @@ export default class Navbar extends React.Component {
   render() {
     var notifications = this.state.notifications;
     return (
-      <div className="component-container">
+      <div className="container">
         <div className="navbar-header">
           <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span className="sr-only">Toggle navigation</span>
