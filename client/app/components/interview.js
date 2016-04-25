03e678dda48f55f4be2944faaf0bcbe2c8ee0d13
Anh Bui <b.ducanh96@gmail.com>
2016-04-25 02:18:54 -0400

    add webrtc

--- client/app/components/interview.js
+++ client/app/components/interview.js
@@ -9,6 +9,9 @@ import {getInterviewSession} from '../server';
 export default class Interview extends React.Component {
   constructor(props) {
     super(props);
+    this.video = true;
+    this.muted = true;
+    this.volume = 1;
     this.state = {interview: {
       problem: {
         title: ""
@@ -31,19 +34,71 @@ export default class Interview extends React.Component {
     this.refresh();
   }
 
+  onVolume(e) {
+    e.preventDefault();
+    this.volume = (this.volume === 0) ? 1 : 0;
+    console.log("toggle volume" + this.volume);
+    this.webrtc.setVolumeForAll(this.volume);
+  }
+
+  onMicro(e) {
+    e.preventDefault();
+    this.muted = (this.muted) ? false : true;
+    console.log("toggle micro" + this.muted);
+    if (this.muted) {
+      this.webrtc.mute();
+    }
+    else {
+      this.webrtc.unmute();
+    }
+  }
+
+  onVideo(e) {
+    e.preventDefault();
+    this.video = (this.video) ? false : true;
+    console.log("toggle video " + this.video);
+    if (this.video) {
+      this.webrtc.resumeVideo();
+    }
+    else {
+      this.webrtc.pauseVideo();
+    }
+  }
+
   render() {
+    this.webrtc = new SimpleWebRTC({
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos
+      remoteVideosEl: 'remotesVideos',
+      // immediately ask for camera access
+      autoRequestMedia: true,
+      media: { video: true, audio: true},
+      url: 'http://project-webrtc.herokuapp.com'
+    });
+    // we have to wait until it's ready
+    this.webrtc.on('readyToCall', function () {
+      // you can name it anything
+      this.webrtc.joinRoom('your awesome room name');
+    }.bind(this));
     return (
       <div className="component-container">
         <div className="row">
           <div className="col-md-4">
-            <VoiceChat />
+            <VoiceChat onVolume={(e) => this.onVolume(e)} onVideo={(e) => this.onVideo(e)} onMicro={(e) => this.onMicro(e)}></VoiceChat>
+            <video height="200" id="localVideo"></video>
+            <div className="remotesVideos" ref={(ref) => this.remotes = ref} id="remotesVideos"></div>
             <Questions interviewquestion={this.state.interview}/>
           </div>
           <CodeEditor interviewerId={this.state.interview.interviewer._id} intervieweeId={this.state.interview.interviewee._id} interviewId={this.props.params.interviewId} />
         </div>
       </div>
-
-
     )
+  }
+
+  componentWillUnmount() {
+    this.webrtc.stopLocalVideo();
+    this.webrtc.leaveRoom();
+    this.webrtc.disconnect();
   }
 }
--- client/app/components/voicechat.js
+++ client/app/components/voicechat.js
@@ -8,14 +8,14 @@ export default class VoiceChat extends React.Component {
 
             <div className="panel panel-default">
               <div className="panel-heading text-center">
-                <button type="button" className="navbar-btn btn btn-default pull-left">
-                  <span className="glyphicon glyphicon-earphone" style={{color: 'red'}}></span>
+                <button type="button" className="btn btn-default pull-left" onClick={(e) => this.props.onVolume(e)}>
+                  <span className="glyphicon glyphicon-volume-off" style={{color: 'red'}}></span>
                 </button>
-                <button type="button" className="navbar-btn btn btn-default">
-                  <span className="glyphicon glyphicon-record"></span>
+                <button type="button" className="btn btn-default" onClick={(e) => this.props.onVideo(e)}>
+                  <span className="glyphicon glyphicon-facetime-video"></span>
                 </button>
-                <button type="button" className="navbar-btn btn btn-default pull-right">
-                  <span className="glyphicon glyphicon-volume-up"></span>
+                <button type="button" className="btn btn-default pull-right" onClick={(e) => this.props.onMicro(e)}>
+                  <span className="glyphicon glyphicon-bullhorn"></span>
                 </button>
               </div>
             </div>
